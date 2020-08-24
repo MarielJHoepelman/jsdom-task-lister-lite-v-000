15931adfe227a7b612a4f80a3cf75f8d3681ede9
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-24 20:39:21 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("#create-task-form")
 
   form.addEventListener('submit', event =>{
-    let input = document.querySelector("#new-task-description").value;
-    let list = document.querySelector("#tasks");
-    list.innerHTML += `<li> ${input} </li>`;
+    // let input = document.querySelector("#new-task-description").value;
+    // let list = document.querySelector("#tasks");
+    // list.innerHTML += `<li> ${input} </li>`;
+    document.querySelector(body).innerHTML ="<h1>pee pee</h>"
     event.preventDefault();
   });
 });
