c10218300a46417847589959b2076454a2761085
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-24 20:57:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+const myFunction = () => {console.log("here")}
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const form = document.querySelector("#create-task-form")
+
 
   form.addEventListener('submit', event =>{
     let input = document.querySelector("#new-task-description").value;
