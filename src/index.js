eece086752c739881af49c62b70c283809399264
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-24 21:02:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const myFunction = (event) => {console.log(event)}
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const form = document.querySelector("#create-task-form")
+  let list = document.querySelector("#tasks");
 
 
   form.addEventListener('submit', event =>{
     let input = document.querySelector("#new-task-description").value;
-    let list = document.querySelector("#tasks");
-    list.innerHTML += `<li> ${input} <button onclick="myFunction()">Click me</button></li>`;
+    list.innerHTML += `<li> ${input} <button>Click me</button></li>`;
     event.preventDefault();
   });
+
+
 });
