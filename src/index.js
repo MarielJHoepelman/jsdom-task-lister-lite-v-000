930918a7f66ad8374fc508a84c385d30dd099884
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-24 23:16:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
       event.target.parentElement.dataset.priority = targetValue;
       event.target.parentElement.style.color = color;
 
-      const tasks = document.querySelector("#tasks");
+      tasks = document.querySelector("#tasks");
 
       const sorted = Array.from(tasks.children).sort((a,b)=> {
         return b.dataset.priority < a.dataset.priority ? 1 : -1
