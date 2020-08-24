a2647c3bcfaaa412b7789e1eee82ab260b69f25d
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-24 21:11:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   list.addEventListener('click', event =>{
-    event.preventDefault();
     if (event.target.nodeName === "BUTTON"){
       event.target.parentElement.remove()
     }
