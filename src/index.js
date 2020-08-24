c45ca6ed9fd7a2710a324cc966d6bf232dc424c8
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-24 20:20:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const form = document.querySelector("#create-task-form")
-  console.log(form)
-  debugger;
+
+  form.addEventListener('submit', event =>{
+
+    debugger
+
+  });
 });
