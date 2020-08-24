a3a4abaccad732fbe0954e636daa8612d1f29a40
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-24 22:09:16 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
   const form = document.querySelector("#create-task-form")
   const list = document.querySelector("#tasks");
 
